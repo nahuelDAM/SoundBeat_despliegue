359c65ce3ae9a6c3d5489475482691ec0571e99a
nahuelDAM <wgf90900@educastur.es>
2025-05-05 19:41:42 +0200

    front agregado

---
+++ frontend/www/chunk-OTRAQYYQ.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-ZGM7264U.js";import"./chunk-WS3OVXI4.js";import"./chunk-HII7ZFLR.js";import"./chunk-Z65EUC5N.js";import"./chunk-OWSRRI5Z.js";import"./chunk-LF5XB4YN.js";import"./chunk-RW4GY4BD.js";export{b as iosTransitionAnimation,a as shadow};
